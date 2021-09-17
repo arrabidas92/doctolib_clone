0c74b65080ac3d091ab6ff21142d5d404d550f1e
Alexandre DUARTE <alexandre.jose.duarte@gmail.com>
2021-09-17 11:29:01 +0100

    add first section of info card

--- Doctolib_compose/app/src/main/java/com/oway/your/way/is/my/way/doctolib_compose/HomeScreen.kt
+++ Doctolib_compose/app/src/main/java/com/oway/your/way/is/my/way/doctolib_compose/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.oway.your.way.`is`.my.way.doctolib_compose
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -7,9 +8,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,10 +17,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.oway.your.way.`is`.my.way.doctolib_compose.ui.theme.Doctolib_composeTheme
@@ -30,7 +35,7 @@ import com.oway.your.way.`is`.my.way.doctolib_compose.ui.theme.Doctolib_composeT
 @Composable
 fun HomeScreen(onSearchBarClick: () -> Unit) {
     LazyColumn(
-       modifier = Modifier.background(MaterialTheme.colors.primaryVariant)
+        modifier = Modifier.background(MaterialTheme.colors.primaryVariant)
     ) {
         item {
             TopBar(modifier = Modifier
@@ -59,6 +64,17 @@ fun HomeScreen(onSearchBarClick: () -> Unit) {
         }
         item {
             HealthProCard()
+        }
+        item {
+            Spacer(modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+            )
+        }
+        item {
+            InfoCard(
+                modifier = Modifier.padding(16.dp)
+            )
         }
     }
 }
@@ -208,5 +224,144 @@ fun HealthProCard() {
             modifier = Modifier
                 .size(26.dp)
         )
+    }
+}
+
+@Composable
+fun InfoCard(modifier: Modifier = Modifier) {
+    Card (
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = modifier
+    ) {
+        Column {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .height(44.dp)
+                    .fillMaxWidth()
+                    .background(
+                        color = Color.Green.copy(alpha = 0.25f),
+                        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp)
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_info_24px),
+                    colorFilter = ColorFilter.tint(color = MaterialTheme.colors.secondaryVariant),
+                    contentDescription = "icon information",
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(
+                    stringResource(id = R.string.informations),
+                    style = MaterialTheme.typography.body2,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Text(
+                stringResource(id = R.string.vaccination_new_hours),
+                color = MaterialTheme.colors.secondaryVariant,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp)
+            )
+            Text(
+                stringResource(id = R.string.vaccination_body),
+                color = MaterialTheme.colors.secondaryVariant,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                style = MaterialTheme.typography.body2
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            AnnotatedClickableText(
+                unclickableText = stringResource(id = R.string.vaccination_knowledge),
+                clickableText = stringResource(id = R.string.mode_emploi),
+                url = stringResource(id = R.string.mode_emploi_url),
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            AnnotatedClickableText(
+                unclickableText = stringResource(id = R.string.stat),
+                clickableText = stringResource(id = R.string.statistics),
+                url = stringResource(id = R.string.stat_url),
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            ButtonCTA(
+                action = stringResource(id = R.string.take_appointment),
+                textColor = Color.White
+            )
+            Spacer(modifier = Modifier.size(24.dp))
+            Divider(modifier = Modifier.padding(start = 16.dp, end = 16.dp))
+            Spacer(modifier = Modifier.size(24.dp))
+        }
+    }
+}
+
+@Composable
+fun ButtonCTA(
+    action: String,
+    textColor: Color
+) {
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primaryVariant),
+        modifier = Modifier
+            .height(44.dp)
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        Text(
+            action.uppercase(),
+            color = textColor,
+            style = MaterialTheme.typography.button,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Composable
+fun AnnotatedClickableText(
+    unclickableText: String,
+    clickableText: String,
+    url: String,
+    modifier: Modifier = Modifier
+) {
+    val uriHandler = LocalUriHandler.current
+    val annotatedText = buildAnnotatedString {
+        append("$unclickableText ")
+
+        // We attach this *URL* annotation to the following content
+        // until `pop()` is called
+        pushStringAnnotation(tag = "URL", annotation = url)
+        withStyle(
+            style = SpanStyle(
+                color = MaterialTheme.colors.primaryVariant,
+                fontWeight = FontWeight.Bold,
+                textDecoration = TextDecoration.Underline
+            )
+        ) {
+            append(clickableText)
+        }
+
+        pop()
+    }
+
+    ClickableText(
+        text = annotatedText,
+        onClick = { offset ->
+            // We check if there is an *URL* annotation attached to the text
+            // at the clicked position
+            annotatedText.getStringAnnotations(tag = "URL", start = offset,
+                end = offset)
+                .firstOrNull()?.let { uriHandler.openUri(url) }
+        },
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InfoCardPreview() {
+    Doctolib_composeTheme {
+        InfoCard()
     }
 }
